0379c4924a2ff4320a2d0c6f8fe06f5c26241d8d
Adiptify <kumar.aditya.prof@gmail.com>
2026-02-03 10:44:04 +0530

    Upadte readme.md of the Exp-3

--- Exp-3/Section-1/README.md
+++ Exp-3/Section-1/README.md
@@ -11,8 +11,6 @@ A project focusing on interactive web elements, including dynamic lists, input h
 ![Home Preview](./src/assets/Screenshot%202026-01-27%20114354.png)
 ![Project Section](./src/assets/Screenshot%202026-01-27%20114442.png)
 ![Details View](./src/assets/Screenshot%202026-01-27%20114500.png)
-![Mobile View 1](./src/assets/Screenshot%202026-01-27%20114522.png)
-![Mobile View 2](./src/assets/Screenshot%202026-01-27%20114537.png)
 
 ## Tech Stack
 - React 19
--- Exp-3/Section-2/src/components/Navbar.jsx
+++ Exp-3/Section-2/src/components/Navbar.jsx
@@ -1,20 +1,15 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <div className="nav-container">
-                <div className="nav-links">
-                    <NavLink to="/" end className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
-                        Profile
-                    </NavLink>
-                    <a href="/#dashboard" className="nav-link">Dashboard</a>
-                    <a href="/#experience" className="nav-link">Experience</a>
-                    <a href="/#skills" className="nav-link">Skills</a>
-                    <a href="/#contact" className="nav-link">Contact</a>
-                </div>
+            <div className="nav-brand">Link Lab</div>
+            <div className="nav-links">
+                <Link to="/" className="nav-link">Home</Link>
+                <Link to="/gallery" className="nav-link">Gallery</Link>
+                <Link to="/info" className="nav-link">Info</Link>
             </div>
         </nav>
     );
